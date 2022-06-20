666a09b39c91ce3d6667a28d85b7bd8cd7b78f8b
ggalactiko <support@galactiko.com>
2022-06-20 08:45:53 -0500

    j

--- AntiScam.py
+++ AntiScam.py
@@ -1,5 +1,4 @@
 import discord
-
 message_content = ''
 last_message = ''
 last_message_content = ''
@@ -7,17 +6,19 @@ spam_counter = 0
 
 __version__ = "1.0.0"
 
-async def AntiScam(message, bot, whitelist, muted_role, verified_role, logs_channel):
-    if message.author.bot:
-        return
+async def AntiScam(message: discord.Message, bot, whitelist, muted_role, logs_channel):
     global message_content, last_message, last_message_content, spam_counter
     message_content = f'{message.author.id}: {message.content}'
     message_content = message_content.replace("'", "`")
     mentions = message.raw_mentions
     # AntiScam-System
+    if message.author.bot and message.author.public_flags == 65536:
+        return
+
     if message_content == last_message_content and message.content != '' and message.author.id not in whitelist:
         spam_counter += 1
         await message.delete()
+
     else:
         last_message = message
         last_message_content = message_content
@@ -30,10 +31,8 @@ async def AntiScam(message, bot, whitelist, muted_role, verified_role, logs_chan
     if spam_counter > 1 and message.author.id not in whitelist:
         spam_counter = 0
         muted = discord.utils.get(message.author.guild.roles, name=muted_role)
-        verified = discord.utils.get(message.author.guild.roles, name=verified_role)
         await last_message.delete()
         await message.author.add_roles(muted)
-        await message.author.remove_roles(verified)
         channel = bot.get_channel(logs_channel)
         await channel.send(f'USUARIO MUTEADO: {message_content}')
     await bot.process_commands(message)
