2041b49f69c4396ebbcd877c98b79de0fad8f140
ggalactiko <support@galactiko.com>
2022-06-22 10:43:56 -0500

    fixed command 'hug' and 'pat'

--- cogs/i.py
+++ cogs/i.py
@@ -10,35 +10,40 @@ class inter(commands.Cog):  # All cogs must inherit from commands.Cog
         self.bot = bot
     
     @commands.command(name='hug')
-    async def hug(self, ctx: commands.Context, member: discord.Member):
+    async def hug(self, ctx: commands.Context, member: discord.Member = None):
         member = member or ctx.message.mentions[0]
-        async with aiohttp.ClientSession() as session:
-            request = await session.get('https://some-random-api.ml/animu/hug')
-            hugjson = await request.json() 
-        embed = discord.Embed(title="", color=discord.Color.green()) 
-        embed.set_image(url=hugjson['link']) 
-        await ctx.send(f"{ctx.author.mention} abrazó a {member.mention}") 
-        await ctx.send(embed=embed)
+        if member == ctx.message.author:
+            await ctx.send("No puedes abrazarte a ti mismo")
+            return
+        else:
+            async with aiohttp.ClientSession() as session:
+                request = await session.get('https://some-random-api.ml/animu/hug')
+                hugjson = await request.json() 
+            embed = discord.Embed(title=f"{ctx.author.mention} abrazó a {member.mention}", color=discord.Color.green()) 
+            embed.set_image(url=hugjson['link']) 
+            await ctx.send(embed=embed)
 
     @commands.command(name='pat')
-    async def pat(self, ctx: commands.Context, member: discord.Member):
+    async def pat(self, ctx: commands.Context, member: discord.Member = None):
         member = member or ctx.message.mentions[0]
-        async with aiohttp.ClientSession() as session:
-            request = await session.get('https://some-random-api.ml/animu/pat')
-            patjson = await request.json() 
-        embed = discord.Embed(title="", color=discord.Color.green()) 
-        embed.set_image(url=patjson['link']) 
-        await ctx.send(f"{ctx.author.mention} acarició a {member.mention}") 
-        await ctx.send(embed=embed) 
+        if member == ctx.message.author:
+            await ctx.send("No puedes acariciarte a ti mismo")
+            return
+        else:
+            async with aiohttp.ClientSession() as session:
+                request = await session.get('https://some-random-api.ml/animu/pat')
+                patjson = await request.json() 
+            embed = discord.Embed(title=f"{ctx.author.mention} acarició a {member.mention}", color=discord.Color.green()) 
+            embed.set_image(url=patjson['link']) 
+            await ctx.send(embed=embed) 
 
     @commands.command(name='wink')
     async def wink(self, ctx: commands.Context):
      async with aiohttp.ClientSession() as session:
       request = await session.get('https://some-random-api.ml/animu/wink')
       winkjson = await request.json() 
-     embed = discord.Embed(title="", color=discord.Color.green()) 
+     embed = discord.Embed(title=f"{ctx.author.mention} guiñó el ojo", color=discord.Color.green()) 
      embed.set_image(url=winkjson['link']) 
-     await ctx.send(f"{ctx.author.mention} guiñó el ojo") 
      await ctx.send(embed=embed) 
 
 async def setup(bot):
