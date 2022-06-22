d25caa85b2618784537bb0e1fb9c0e0596d91e44
ggalactiko <support@galactiko.com>
2022-06-21 19:28:59 -0500

    .

--- cogs/i.py
+++ cogs/i.py
@@ -11,23 +11,25 @@ class inter(commands.Cog):  # All cogs must inherit from commands.Cog
     
     @commands.command(name='hug')
     async def hug(self, ctx: commands.Context, member: discord.Member):
-     async with aiohttp.ClientSession() as session:
-      request = await session.get('https://some-random-api.ml/animu/hug')
-      hugjson = await request.json() 
-     embed = discord.Embed(title="", color=discord.Color.green()) 
-     embed.set_image(url=hugjson['link']) 
-     await ctx.send(f"{ctx.author.mention} abrazó a {member.mention}") 
-     await ctx.send(embed=embed) 
+        member = member or ctx.message.mentions[0]
+        async with aiohttp.ClientSession() as session:
+            request = await session.get('https://some-random-api.ml/animu/hug')
+            hugjson = await request.json() 
+        embed = discord.Embed(title="", color=discord.Color.green()) 
+        embed.set_image(url=hugjson['link']) 
+        await ctx.send(f"{ctx.author.mention} abrazó a {member.mention}") 
+        await ctx.send(embed=embed) 
 
     @commands.command(name='pat')
     async def pat(self, ctx: commands.Context, member: discord.Member):
-     async with aiohttp.ClientSession() as session:
-      request = await session.get('https://some-random-api.ml/animu/pat')
-      patjson = await request.json() 
-     embed = discord.Embed(title="", color=discord.Color.green()) 
-     embed.set_image(url=patjson['link']) 
-     await ctx.send(f"{ctx.author.mention} acarició a {member.mention}") 
-     await ctx.send(embed=embed) 
+        member = member or ctx.message.mentions[0]
+        async with aiohttp.ClientSession() as session:
+            request = await session.get('https://some-random-api.ml/animu/pat')
+            patjson = await request.json() 
+        embed = discord.Embed(title="", color=discord.Color.green()) 
+        embed.set_image(url=patjson['link']) 
+        await ctx.send(f"{ctx.author.mention} acarició a {member.mention}") 
+        await ctx.send(embed=embed) 
 
     @commands.command(name='wink')
     async def wink(self, ctx: commands.Context):
